4e959db6fcef4c15d2e61fb26f4bedbca62f2daf
Xs <xsmumu@gmail.com>
2015-01-25 15:26:15 +0800

    Update openwrt_adbyby.sh

--- openwrt_adbyby.sh
+++ openwrt_adbyby.sh
@@ -4,17 +4,22 @@ echo "Welcome to use Auto Adbyby Install for 7620n"
 echo "Author: Xs"
 echo "Email: jye1107@qq.com"
 echo " "
-echo "Now will download adbyby package and install"
+echo "Now will download adbyby package"
 wget http://info.adbyby.com/download/7620n.tar.gz
 mkdir adbyby
 tar -xzvf 7620n.tar.gz -C adbyby
+echo "Remove old version"
 rm -rf /usr/bin/adbyby
-mv adbyby /usr/bin/adbyby
-rm -rf 7620n.tar.gz
-chmod a+x /usr/bin/adbyby/adbyby
-/usr/bin/adbyby/adbyby &
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8118
 rm -rf /etc/init.d/adbyby
+echo "Install adbyby"
+mv adbyby /usr/bin/adbyby
 mv init_adbyby /etc/init.d/adbyby
 chmod a+x /etc/init.d/adbyby
+chmod a+x /usr/bin/adbyby/adbyby
+echo "Start service"
+/usr/bin/adbyby/adbyby &
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8118
+echo "Clean tmp"
+rm -rf openwrt_adbyby.sh
+rm -rf 7620n.tar.gz
 echo "Done!"
